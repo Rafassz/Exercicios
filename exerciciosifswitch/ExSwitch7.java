c8ea642491ad22716ed3a6436808fc3789345048
Rafassz <rafassz.aps@gmail.com>
2024-11-09 20:06:48 -0300

    Atividades Prática

--- exerciciosifswitch/ExSwitch7.java
+++ exerciciosifswitch/ExSwitch7.java
@@ -5,17 +5,34 @@ import java.util.Scanner;
 public class ExSwitch7 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
-		int idade;
-		
-		
+		int idade, x = 0;
+
 		System.out.println("Digite sua idade: ");
 		idade = scanner.nextInt();
-		
-		switch(true) {
-		case (idade <= 12 && idade > 0):
-			System.out.println("Você é uma Criança!!");
+		if (idade > 0 && idade<= 12) {
+			x = 1;
+		}else if(idade > 12 && idade <= 20) {
+			x = 2;
+		}else if(idade > 20 && idade <= 59) {
+			x = 3;
+		}else if(idade >= 60) {
+			x = 4;
+		}
+
+		switch (x) {
+		case 1:
+			System.out.println("Criança");
+			break;
+		case 2:
+			System.out.println("Adolescente");
+			break;
+		case 3:
+			System.out.println("Adulto");
+			break;
+		case 4:
+			System.out.println("Idoso");
 			break;
 		}
 
